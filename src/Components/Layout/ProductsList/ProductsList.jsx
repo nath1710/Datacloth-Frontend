dc34760ed376d3782f2768a556eed7dd47e13763
Nath <115433779+nath1710@users.noreply.github.com>
2025-05-10 22:00:35 +0000

    Products working

--- src/Components/Layout/ProductsList/ProductsList.jsx
+++ src/Components/Layout/ProductsList/ProductsList.jsx
@@ -2,116 +2,116 @@ import React, { useEffect, useState } from 'react'
 import ProductsListCard from '../../ProductsListCard/ProductsListCard';
 import ProductsFilters from '../../Filters/ProductsFilters';
 
-// const products = [
-//   {
-//     id: 1,
-//     name: "Yellow Coat",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://i5.walmartimages.com/seo/TAGOLD-Raincoat-Women-Winter-Jacket-Womens-Plus-Size-Hooded-Trench-Coats-Women-Solid-Rain-Jacket-Outdoor-Windproof-Top-Lined-Windbreaker-Travel-Yello_73e01008-3221-4631-8da8-7cb67269cb2a.1e1faa72451c0ad0af84ab81b9d20e93.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
-//   },
-//   {
-//     id: 2,
-//     name: "White Shirt",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://cafe24img.poxo.com/scause/web/product/big/202402/5eafbb04b2b89c6f7787f571997924e5.jpg"
-//   },
-//   {
-//     id: 3,
-//     name: "Blue Jean",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://www.shutterstock.com/image-photo/fashion-trendy-womens-jeans-isolated-600nw-2466839305.jpg"
-//   },
-//   {
-//     id: 4,
-//     name: "Yellow Coat",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://i5.walmartimages.com/seo/TAGOLD-Raincoat-Women-Winter-Jacket-Womens-Plus-Size-Hooded-Trench-Coats-Women-Solid-Rain-Jacket-Outdoor-Windproof-Top-Lined-Windbreaker-Travel-Yello_73e01008-3221-4631-8da8-7cb67269cb2a.1e1faa72451c0ad0af84ab81b9d20e93.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
-//   },
-//   {
-//     id: 5,
-//     name: "White Shirt",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://cafe24img.poxo.com/scause/web/product/big/202402/5eafbb04b2b89c6f7787f571997924e5.jpg"
-//   },
-//   {
-//     id: 6,
-//     name: "Blue Jean",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://www.shutterstock.com/image-photo/fashion-trendy-womens-jeans-isolated-600nw-2466839305.jpg"
-//   },
-//   {
-//     id: 7,
-//     name: "Yellow Coat",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://i5.walmartimages.com/seo/TAGOLD-Raincoat-Women-Winter-Jacket-Womens-Plus-Size-Hooded-Trench-Coats-Women-Solid-Rain-Jacket-Outdoor-Windproof-Top-Lined-Windbreaker-Travel-Yello_73e01008-3221-4631-8da8-7cb67269cb2a.1e1faa72451c0ad0af84ab81b9d20e93.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
-//   },
-//   {
-//     id: 8,
-//     name: "White Shirt",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://cafe24img.poxo.com/scause/web/product/big/202402/5eafbb04b2b89c6f7787f571997924e5.jpg"
-//   },
-//   {
-//     id: 9,
-//     name: "Blue Jean",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://www.shutterstock.com/image-photo/fashion-trendy-womens-jeans-isolated-600nw-2466839305.jpg"
-//   },
-//   {
-//     id: 10,
-//     name: "Yellow Coat",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://i5.walmartimages.com/seo/TAGOLD-Raincoat-Women-Winter-Jacket-Womens-Plus-Size-Hooded-Trench-Coats-Women-Solid-Rain-Jacket-Outdoor-Windproof-Top-Lined-Windbreaker-Travel-Yello_73e01008-3221-4631-8da8-7cb67269cb2a.1e1faa72451c0ad0af84ab81b9d20e93.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
-//   },
-//   {
-//     id: 11,
-//     name: "White Shirt",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://cafe24img.poxo.com/scause/web/product/big/202402/5eafbb04b2b89c6f7787f571997924e5.jpg"
-//   },
-//   {
-//     id: 12,
-//     name: "Blue Jean",
-//     creationDate: "20h:46m:30s",
-//     category: "Bank Offer",
-//     price: "149",
-//     stock: 45,
-//     image: "https://www.shutterstock.com/image-photo/fashion-trendy-womens-jeans-isolated-600nw-2466839305.jpg"
-//   },
-// ];
+const products = [
+  {
+    id: 1,
+    name: "Yellow Coat",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://i5.walmartimages.com/seo/TAGOLD-Raincoat-Women-Winter-Jacket-Womens-Plus-Size-Hooded-Trench-Coats-Women-Solid-Rain-Jacket-Outdoor-Windproof-Top-Lined-Windbreaker-Travel-Yello_73e01008-3221-4631-8da8-7cb67269cb2a.1e1faa72451c0ad0af84ab81b9d20e93.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+  },
+  {
+    id: 2,
+    name: "White Shirt",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://cafe24img.poxo.com/scause/web/product/big/202402/5eafbb04b2b89c6f7787f571997924e5.jpg"
+  },
+  {
+    id: 3,
+    name: "Blue Jean",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://www.shutterstock.com/image-photo/fashion-trendy-womens-jeans-isolated-600nw-2466839305.jpg"
+  },
+  {
+    id: 4,
+    name: "Yellow Coat",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://i5.walmartimages.com/seo/TAGOLD-Raincoat-Women-Winter-Jacket-Womens-Plus-Size-Hooded-Trench-Coats-Women-Solid-Rain-Jacket-Outdoor-Windproof-Top-Lined-Windbreaker-Travel-Yello_73e01008-3221-4631-8da8-7cb67269cb2a.1e1faa72451c0ad0af84ab81b9d20e93.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+  },
+  {
+    id: 5,
+    name: "White Shirt",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://cafe24img.poxo.com/scause/web/product/big/202402/5eafbb04b2b89c6f7787f571997924e5.jpg"
+  },
+  {
+    id: 6,
+    name: "Blue Jean",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://www.shutterstock.com/image-photo/fashion-trendy-womens-jeans-isolated-600nw-2466839305.jpg"
+  },
+  {
+    id: 7,
+    name: "Yellow Coat",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://i5.walmartimages.com/seo/TAGOLD-Raincoat-Women-Winter-Jacket-Womens-Plus-Size-Hooded-Trench-Coats-Women-Solid-Rain-Jacket-Outdoor-Windproof-Top-Lined-Windbreaker-Travel-Yello_73e01008-3221-4631-8da8-7cb67269cb2a.1e1faa72451c0ad0af84ab81b9d20e93.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+  },
+  {
+    id: 8,
+    name: "White Shirt",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://cafe24img.poxo.com/scause/web/product/big/202402/5eafbb04b2b89c6f7787f571997924e5.jpg"
+  },
+  {
+    id: 9,
+    name: "Blue Jean",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://www.shutterstock.com/image-photo/fashion-trendy-womens-jeans-isolated-600nw-2466839305.jpg"
+  },
+  {
+    id: 10,
+    name: "Yellow Coat",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://i5.walmartimages.com/seo/TAGOLD-Raincoat-Women-Winter-Jacket-Womens-Plus-Size-Hooded-Trench-Coats-Women-Solid-Rain-Jacket-Outdoor-Windproof-Top-Lined-Windbreaker-Travel-Yello_73e01008-3221-4631-8da8-7cb67269cb2a.1e1faa72451c0ad0af84ab81b9d20e93.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+  },
+  {
+    id: 11,
+    name: "White Shirt",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://cafe24img.poxo.com/scause/web/product/big/202402/5eafbb04b2b89c6f7787f571997924e5.jpg"
+  },
+  {
+    id: 12,
+    name: "Blue Jean",
+    creationDate: "20h:46m:30s",
+    category: "Bank Offer",
+    price: "149",
+    stock: 45,
+    image: "https://www.shutterstock.com/image-photo/fashion-trendy-womens-jeans-isolated-600nw-2466839305.jpg"
+  },
+];
 
 function ProductsList() {
   const [products, setProducts] = useState([]);
